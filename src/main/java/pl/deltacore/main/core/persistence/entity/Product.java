b72982fd7e3365762d0a94d60ffe20530bc5ea4a
Oskar Kamiński <oskar@deltacore.pl>
2014-02-13 00:12:29 +0100

    Change type

--- src/main/java/pl/deltacore/main/core/persistence/entity/Product.java
+++ src/main/java/pl/deltacore/main/core/persistence/entity/Product.java
@@ -13,7 +13,7 @@ public class Product {
 	@Id
     @Column(name="id")
     @GeneratedValue
-    private Integer id;
+    private Long id;
 	
 	@Column(name="name")
 	private String name;
@@ -21,11 +21,11 @@ public class Product {
 	@Column(name="description")
 	private String description;
 	
-	public Integer getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
